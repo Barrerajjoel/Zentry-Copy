214e73230486a81e9ecd1f58a4dd013044f54ca0
joel barrera <barrerajoelj@gmail.com>
2025-03-12 16:12:40 -0400

    Added fonts and color themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <main>
-      <h1 className='font-zentry'>Hello World</h1>  
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
     </main>
   )
 }
